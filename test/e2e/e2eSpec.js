b11a63d15eee481a7e10a7d3a6047ed78c1b7be5
Masa <us10096698@yahoo.com>
2016-02-22 23:13:08 +0900

    [#114206889] Refactor e2e specs

--- test/e2e/e2eSpec.js
+++ test/e2e/e2eSpec.js
@@ -1,5 +1,4 @@
 'use strict';
-var server = require('../../server');
 
 describe('GET /', function() {
 
@@ -9,17 +8,17 @@ describe('GET /', function() {
   });
 
   it('should return index page with greeting', function() {
-    expect(element(by.id('greeting')).getText()).toEqual('hello, world!');
+    expect($('#greeting').getText()).toBe('hello, world!');
   });
 
   it('should update tagline when call button clicked', function() {
-    element(by.id('call')).click();
-    expect(element(by.id('tagline')).getText()).toEqual('Did you call me?');
+    $('#call').click();
+    expect($('#tagline').getText()).toBe('Did you call me?');
   });
 
   it('should update tagline when AJAX call button clicked', function() {
-    element(by.id('ajaxcall')).click();
-    expect(element(by.id('tagline')).getText()).toEqual('Say hello from server! Your query: Did you call me?');
+    $('#ajaxcall').click();
+    expect($('#tagline').getText()).toBe('Say hello from server! Your query: Did you call me?');
   });
 
   function disableAnimation() {
